75994fa450f87902017dcd14aae91edd15f5f682
Gorgamol <Gorgamol@users.noreply.github.com>
2026-02-03 12:57:57 +0100

    Fixed migration query

--- migrations/003_create_task_categories.sql
+++ migrations/003_create_task_categories.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS task_categories (
     task_id INTEGER NOT NULL,
     category_id INTEGER NOT NULL,
-    PRIMARY KEY (task_id, category_id)
+    PRIMARY KEY (task_id, category_id),
     FOREIGN KEY (task_id) REFERENCES tasks(id) ON DELETE CASCADE,
     FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
 );
